7278754ace1285602bd4db048484633a084f6001
Omar-eAlessa <omar.alessa2001@gmail.com>
2024-04-18 11:57:11 +0300

    msg

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import './output.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center text-5xl font-mono font-bold py-5'>FAQ\Questions</h1>
+      <div className='w-64 h-64 bg-indigo-600 rounded-md text-center my-6 mx-auto'>
+        <h2 className="text-white text-xl pt-24">Frequently Asked Questions</h2>
+        <div className=' rounded-lg text-white space-y-8 text-center mx-2 ml-20'>
+
+           
+          {/* <div className='bg-black w-24 h-7 rounded text-white items-center justify-center'>ss</div> */}
+          {/* <div className='bg-black w-24 h-7 rounded text-white items-center justify-center'>ss</div> */}
+        </div>
+      </div>
+    </> 
+  );
+} 
+
+const questions = [
+  {
+    id: 1 , 
+    title: 'Is this a good product?',
+    info: 'Lorem ipsum dolor'
+  },
+  {
+    id: 2 , 
+    title: 'How much is it?',
+    info: 'Lorem ipsum dolor'
+  },
+  {
+    id: 3 , 
+    title: 'Where is it?',
+    info: 'Lorem ipsum dolor'
+  },
+]
+
+export default App;
